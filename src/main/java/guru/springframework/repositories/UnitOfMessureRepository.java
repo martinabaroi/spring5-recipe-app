8e99136449470e943a5978343ae044751bc7ad6d
martinabaroi <109995362+martinabaroi@users.noreply.github.com>
2022-09-06 10:30:55 -0400

    repo is created (#6)

---
+++ src/main/java/guru/springframework/repositories/UnitOfMessureRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.domain.UnitofMessure;
+
+
+
+public interface UnitOfMessureRepository extends CrudRepository<UnitofMessure, Long> {
+
+}
